aeb4c41f37e9f911b721c0e67bb4b7fc3d7f69ca
muppet0x <163330499+muppet0x@users.noreply.github.com>
2024-06-21 04:22:02 +0200

    update variables

--- main.rs
+++ main.rs
@@ -2,34 +2,34 @@ use actix_web::{web, App, HttpServer, HttpResponse, Responder};
 use dotenv::dotenv;
 use std::env;
 
-async fn add_inventory_item(item: web::Json<InventoryItem>) -> impl Responder {
-    HttpResponse::Ok().json({ "message": "Item added successfully", "item": item })
+async fn create_inventory_item(new_item: web::Json<InventoryItem>) -> impl Responder {
+    HttpResponse::Ok().json({ "message": "Item added successfully", "item": new_item })
 }
 
-async fn get_inventory_items() -> impl Responder {
+async fn list_all_inventory_items() -> impl Responder {
     HttpResponse::Ok().json({ "message": "Inventory List", "items": vec!["Item1", "Item2"] })
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
-struct InventoryItem {
+struct Inventory  Item {
     name: String,
     quantity: i32,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    dotenv().ok(); // Load the .env file if exists
+    dotenv().ok(); 
 
-    let server_url = env::var("SERVER_URL").unwrap_or_else(|_| String::from("127.0.0.1:8080"));
+    let service_endpoint = env::var("SERVER_URL").unwrap_or_else(|_| String::from("127.0.0.1:8080"));
 
-    println!("Starting server at {}", server_url);
+    println!("Starting server at {}", service_endpoint);
 
     HttpServer::new(|| {
         App::new()
-            .route("/inventory", web::post().to(add_inventory_item))
-            .route("/inventory", web::get().to(get_inventory_items))
+            .route("/inventory", web::post().to(create_inventory_item))
+            .route("/inventory", web::get().to(list_all_inventory_items))
     })
-    .bind(&server_url)?
+    .bind(&service_endpoint)?
     .run()
     .await
 }
